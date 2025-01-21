152e07b062a4120a25c7e988693a93c88e4be405
varshapanda <varsha.vijayashwini@kalvium.community>
2025-01-21 12:47:30 +0530

    completed-Designing a component for your entity - Practice assignment #2

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,10 @@
 // write button card here
+
+import React from 'react';
+
+const ViewProductButton=()=>{
+    return (
+        <button style={{ background: '#ffffff', color: '#000000', padding: '10px 20px', borderRadius: '8px' }}><b>View Product</b></button>
+    )
+}
+export default ViewProductButton;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,20 @@
 // write product card here
+import ViewProductButton from "./button";
+
+const Productcard=()=>{
+    const productImage = "https://m.media-amazon.com/images/I/316ArzLeJ2L._SX300_SY300_QL70_FMwebp_.jpg";
+    const productName = "Apple MacBook Air Laptop";
+    const productPrice = "$1,085.37";
+    
+    return(
+        <div>
+            < img src ={productImage} alt={productName}/>
+            <p><b>{productName}</b></p>
+            <p><b>{productPrice}</b></p>
+            <ViewProductButton/>
+        </div>
+    )
+
+}
+
+export default Productcard;
